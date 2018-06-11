d468d05ac451da293cfaa5027b0bc240efc7038c
Brendan Gerardi <brendan.gerardi@gmail.com>
2018-06-11 12:50:52 -0500

    nav bar functionality

---
+++ public/client.js
@@ -0,0 +1,19 @@
+$(function () {
+
+    //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+
+    var dropDown = document.getElementsByClassName("dropDownBtn");
+    var i;
+
+    for (i = 0; i < dropDown.length; i++) {
+        dropDown[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var dropDownContent = this.nextElementSibling;
+            if (dropDownContent.style.display === "block") {
+                dropDownContent.style.display = "none";
+            } else {
+                dropDownContent.style.display = "block";
+            }
+        });
+    }
+})
