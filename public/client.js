9ce42d4ad50171a808bfed7e49b002e1a51260b3
Brendan Gerardi <brendan.gerardi@gmail.com>
2018-06-21 20:12:23 -0500

    triggers for skills section

--- public/client.js
+++ public/client.js
@@ -16,22 +16,65 @@ $(function () {
     }
 })
 
-$(function () {
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
+//$(function () {
+//    var coll = document.getElementsByClassName("collapsible");
+//    var i;
+//
+//    for (i = 0; i < coll.length; i++) {
+//        coll[i].addEventListener("click", function () {
+//            this.classList.toggle("active");
+//            var content = this.nextElementSibling;
+//            if (content.style.maxHeight) {
+//                content.style.maxHeight = null;
+//            } else {
+//                content.style.maxHeight = content.scrollHeight + "px";
+//            }
+//        });
+//    }
+//})
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
-    }
-})
+//$(function () {
+//    let ids = '';
+//    let dropDown = $(".fadeContainer");
+//    let i;
+//    console.log(dropDown);
+//
+//    for (i = 0; i < dropDown.length; i++) {
+//        console.log(this.dropDown);
+//        dropDown[i].addEventListener("click", function () {
+//            $(".skillsInnerSection").show();
+//                        this.classList.toggle("active");
+//                        let dropDownContent = this.lastElementChild;
+//                        console.log(this.dropDownContent);
+//                        if (dropDownContent.style.display === "block") {
+//                            dropDownContent.style.display = "none";
+//                        } else {
+//                            dropDownContent.style.display = "block";
+//                        }
+//        });
+//
+//
+//
+//    };
+//
+//        });
+//});
+
+
+//    dropDown.on("mouseout", function () {
+//        $(this).
+//    });
+
+
+//
+//function myFunction() {
+//    var x = document.getElementsByClassName("skillsInnerSection");
+//    if (x.style.display === "none") {
+//        x.style.display = "block";
+//    } else {
+//        x.style.display = "none";
+//    };
+//}
 
 
 
@@ -46,6 +89,7 @@ $(document).ready(function () {
     $(".projects").show();
     $(".contact").show();
     $(".footerInfo").show();
+    $(".skillsInnerSection").hide();
 });
 
 
@@ -72,9 +116,22 @@ $(document).on("click", ".close", function (event) {
 });
 
 
-
 //hover for project images
 $("div.codeButton").mouseenter(function (event) {
+        event.preventDefault();
+        $(".code", this).css("display", "inline-block");
+        $(".demo", this).css("display", "inline-block");
+        $("img", this).css({
+            "opacity": "0.1",
+            "border": "2px solid white",
+            "-webkit-transform": "scale(1.1)",
+            "-moz-transform": "scale(1.1)",
+            "-ms-transform": "scale(1.1)",
+            "-o-transform": "scale(1.1)",
+            "transform": "scale(1.1)"
+        });
+    })
+    .mouseup(function (event) {
         event.preventDefault();
         $(".code", this).css("display", "inline-block");
         $(".demo", this).css("display", "inline-block");
@@ -101,92 +158,172 @@ $("div.codeButton").mouseenter(function (event) {
             "-o-transform": "scale(1)",
             "transform": "scale(1)"
         });
-    });
+    })
+
+
+
+$(document).on("click", "#languages", function () {
+    event.preventDefault();
+    $(".languages").show();
+    $(".front").hide();
+    $(".back").hide();
+    $(".dataB").hide();
+    $(".testing").hide();
+    $(".development").hide();
+    $(".miscellaneous").hide();
+});
+
+$(document).on("click", "#frontend", function () {
+    event.preventDefault();
+    $(".languages").hide();
+    $(".front").show();
+    $(".back").hide();
+    $(".dataB").hide();
+    $(".testing").hide();
+    $(".development").hide();
+    $(".miscellaneous").hide();
+});
+
+$(document).on("click", "#backend", function () {
+    event.preventDefault();
+    $(".languages").hide();
+    $(".front").hide();
+    $(".back").show();
+    $(".dataB").hide();
+    $(".testing").hide();
+    $(".development").hide();
+    $(".miscellaneous").hide();
+});
+
+$(document).on("click", "#database", function () {
+    event.preventDefault();
+    $(".languages").hide();
+    $(".front").hide();
+    $(".back").hide();
+    $(".dataB").show();
+    $(".testing").hide();
+    $(".development").hide();
+    $(".miscellaneous").hide();
+});
+
+$(document).on("click", "#testing", function () {
+    event.preventDefault();
+    $(".languages").hide();
+    $(".front").hide();
+    $(".back").hide();
+    $(".dataB").hide();
+    $(".testing").show();
+    $(".development").hide();
+    $(".miscellaneous").hide();
+});
+
+$(document).on("click", "#development", function () {
+    event.preventDefault();
+    $(".languages").hide();
+    $(".front").hide();
+    $(".back").hide();
+    $(".dataB").hide();
+    $(".testing").hide();
+    $(".development").show();
+    $(".miscellaneous").hide();
+});
+
+$(document).on("click", "#miscellaneous", function () {
+    event.preventDefault();
+    $(".languages").hide();
+    $(".front").hide();
+    $(".back").hide();
+    $(".dataB").hide();
+    $(".testing").hide();
+    $(".development").hide();
+    $(".miscellaneous").show();
+});
+
 
 
 
 
 //triggers for skill buttons
-$(document).ready(function () {
-    $("#toggle").hide();
-    $(".close").hide();
-    $(".welcomeHeader").hide();
-    $(".headerSection").show();
-    $(".aboutMe").show();
-    $(".skills").show();
-    $(".projects").show();
-    $(".contact").show();
-    $(".footerInfo").show();
-});
-
-$(document).ready(function () {
-    $("#toggle").hide();
-    $(".close").hide();
-    $(".welcomeHeader").hide();
-    $(".headerSection").show();
-    $(".aboutMe").show();
-    $(".skills").show();
-    $(".projects").show();
-    $(".contact").show();
-    $(".footerInfo").show();
-});
-
-$(document).ready(function () {
-    $("#toggle").hide();
-    $(".close").hide();
-    $(".welcomeHeader").hide();
-    $(".headerSection").show();
-    $(".aboutMe").show();
-    $(".skills").show();
-    $(".projects").show();
-    $(".contact").show();
-    $(".footerInfo").show();
-});
-
-$(document).ready(function () {
-    $("#toggle").hide();
-    $(".close").hide();
-    $(".welcomeHeader").hide();
-    $(".headerSection").show();
-    $(".aboutMe").show();
-    $(".skills").show();
-    $(".projects").show();
-    $(".contact").show();
-    $(".footerInfo").show();
-});
-
-$(document).ready(function () {
-    $("#toggle").hide();
-    $(".close").hide();
-    $(".welcomeHeader").hide();
-    $(".headerSection").show();
-    $(".aboutMe").show();
-    $(".skills").show();
-    $(".projects").show();
-    $(".contact").show();
-    $(".footerInfo").show();
-});
-
-$(document).ready(function () {
-    $("#toggle").hide();
-    $(".close").hide();
-    $(".welcomeHeader").hide();
-    $(".headerSection").show();
-    $(".aboutMe").show();
-    $(".skills").show();
-    $(".projects").show();
-    $(".contact").show();
-    $(".footerInfo").show();
-});
-
-$(document).ready(function () {
-    $("#toggle").hide();
-    $(".close").hide();
-    $(".welcomeHeader").hide();
-    $(".headerSection").show();
-    $(".aboutMe").show();
-    $(".skills").show();
-    $(".projects").show();
-    $(".contact").show();
-    $(".footerInfo").show();
-});
+//$(document).ready(function () {
+//    $("#toggle").hide();
+//    $(".close").hide();
+//    $(".welcomeHeader").hide();
+//    $(".headerSection").show();
+//    $(".aboutMe").show();
+//    $(".skills").show();
+//    $(".projects").show();
+//    $(".contact").show();
+//    $(".footerInfo").show();
+//});
+//
+//$(document).ready(function () {
+//    $("#toggle").hide();
+//    $(".close").hide();
+//    $(".welcomeHeader").hide();
+//    $(".headerSection").show();
+//    $(".aboutMe").show();
+//    $(".skills").show();
+//    $(".projects").show();
+//    $(".contact").show();
+//    $(".footerInfo").show();
+//});
+//
+//$(document).ready(function () {
+//    $("#toggle").hide();
+//    $(".close").hide();
+//    $(".welcomeHeader").hide();
+//    $(".headerSection").show();
+//    $(".aboutMe").show();
+//    $(".skills").show();
+//    $(".projects").show();
+//    $(".contact").show();
+//    $(".footerInfo").show();
+//});
+//
+//$(document).ready(function () {
+//    $("#toggle").hide();
+//    $(".close").hide();
+//    $(".welcomeHeader").hide();
+//    $(".headerSection").show();
+//    $(".aboutMe").show();
+//    $(".skills").show();
+//    $(".projects").show();
+//    $(".contact").show();
+//    $(".footerInfo").show();
+//});
+//
+//$(document).ready(function () {
+//    $("#toggle").hide();
+//    $(".close").hide();
+//    $(".welcomeHeader").hide();
+//    $(".headerSection").show();
+//    $(".aboutMe").show();
+//    $(".skills").show();
+//    $(".projects").show();
+//    $(".contact").show();
+//    $(".footerInfo").show();
+//});
+//
+//$(document).ready(function () {
+//    $("#toggle").hide();
+//    $(".close").hide();
+//    $(".welcomeHeader").hide();
+//    $(".headerSection").show();
+//    $(".aboutMe").show();
+//    $(".skills").show();
+//    $(".projects").show();
+//    $(".contact").show();
+//    $(".footerInfo").show();
+//});
+//
+//$(document).ready(function () {
+//    $("#toggle").hide();
+//    $(".close").hide();
+//    $(".welcomeHeader").hide();
+//    $(".headerSection").show();
+//    $(".aboutMe").show();
+//    $(".skills").show();
+//    $(".projects").show();
+//    $(".contact").show();
+//    $(".footerInfo").show();
+//});
